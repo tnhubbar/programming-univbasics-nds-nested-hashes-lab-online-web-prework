010a3475a716c61d42096a0ad87223c579704689
Taryn Johnson <taryn.johnson919@gmail.com>
2019-11-11 23:27:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,7 @@ def assembled_aoh
     { name:  "Don Gately", occupation: "Live-in Staff" },
     { name:  "Joelle van Dyne", occupation: "Radio Personality" }, 
     { name:  "Pat Monteseian", occupation: "Staff" }, 
-    KATE_G, 
+    { name:  "Kate Gompert", occupation: "None" },
     BRUCE_G]
 end
 
