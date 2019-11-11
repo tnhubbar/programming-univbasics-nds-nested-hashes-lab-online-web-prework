fe4c31f8caae2a6e874f0b71ea3d674167a4a44d
Taryn Johnson <taryn.johnson919@gmail.com>
2019-11-11 23:27:42 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,7 +14,7 @@ def assembled_aoh
     { name:  "Joelle van Dyne", occupation: "Radio Personality" }, 
     { name:  "Pat Monteseian", occupation: "Staff" }, 
     { name:  "Kate Gompert", occupation: "None" },
-    BRUCE_G]
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }]
 end
 
 def literal_aoh
